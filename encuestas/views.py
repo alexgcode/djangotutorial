8ec2571f35b101790cb8535a5ac690e97d5209e8
ALEXGF <alexgf2703@gmail.com>
2021-09-07 18:39:50 -0500

    adding vote and result views

--- encuestas/views.py
+++ encuestas/views.py
@@ -1,7 +1,8 @@
-from .models import Question
+from .models import Question, Choice
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
-from django.http import Http404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
 # Create your views here.
 
 def index(request):
@@ -14,8 +15,25 @@ def detail(request, question_id):
     return render(request, 'encuestas/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'encuestas/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    #has the problem of  Avoiding race conditions using F() 
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'encuestas/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        #only executed if no exception was raised
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('encuestas:results', args=(question.id,)))
